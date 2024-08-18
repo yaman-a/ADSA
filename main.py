531e575375f451b4b1fa8186c46cf52e75777d91
yaman-a <yaman.ashqar05@gmail.com>
2024-08-18 19:07:57 +0930

    please work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def addSchoolMethod(I1, I2):
+
     num1 = int(I1, base)
     num2 = int(I2, base)
+
     sumResult = num1 + num2
     return sumResult
 
@@ -8,25 +10,21 @@ def karatsuba(x, y):
     num1 = int(x, base)
     num2 = int(y, base)
 
-    if num1 < 10 or num2 < 10:
+    if num1 < base and num2 < base:
         return num1 * num2
 
-    # Adjust the lengths
     max_len = max(len(x), len(y))
     x = x.zfill(max_len)
     y = y.zfill(max_len)
 
-    # Split x and y
     split_position = max_len // 2
     x1, x0 = x[:-split_position], x[-split_position:]
     y1, y0 = y[:-split_position], y[-split_position:]
 
-    # Recursively calculate z2, z1, and z0
     z2 = karatsuba(x1, y1)
     z0 = karatsuba(x0, y0)
     z1 = karatsuba(addSchoolMethod(x1, x0), addSchoolMethod(y1, y0)) - z2 - z0
 
-    # Combine results using base ** (half_length)
     return z2 * (base ** (2 * split_position)) + z1 * (base ** split_position) + z0
 
 def convertToBase(number, base):
