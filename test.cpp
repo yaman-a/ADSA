03f40e0015c198761b7526464f0f7712e22b0026
yaman-a <yaman.ashqar05@gmail.com>
2024-09-12 16:18:53 +0930

    Made node

---
+++ test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+    int height;
+
+    Node(int k) {
+        val = k;
+        left = nullptr;
+        right = nullptr;
+        height = 1;
+    }
+};
+
+int height(Node* n) {
+    if (!n) {
+        return 0;
+    }
+    return n->height;
+}
+
+// Level order traversal, mainly for testing purposes
+void levelOrder(Node* root) {
+    if (!root) {
+        return;
+    }
+    
+    std::queue<Node*> q;
+    Node* curr;
+    q.push(root);
+    q.push(NULL);
+
+    while (q.size() > 1) {
+        curr = q.front();
+        q.pop();
+
+        if (!curr) {
+            q.push(NULL);
+            std::cout << std::endl;
+        } else {
+            if (curr->left) {
+                q.push(curr->left);
+            }
+            if (curr->right) {
+                q.push(curr->right);
+            }
+
+            std::cout << curr->val << " ";
+
+        }
+    }
+}
+
+int main() {
+    Node* root = new Node(5);
+
+    root->left = new Node(6);
+    root->right = new Node(4);
+
+    levelOrder(root);
+
+    return 0;
+}
+
