e6db6c4acfea7a3071917396dd5d0b2d10290415
yaman-a <yaman.ashqar05@gmail.com>
2024-09-12 17:19:35 +0930

    Works

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def rightRotate(a):
     a.height = 1 + max(height(a.left), height(a.right))
     b.height = 1 + max(height(b.left), height(b.right))
 
-    return a
+    return b
 
 def leftRotate(x):
     y = x.right
@@ -29,6 +29,8 @@ def leftRotate(x):
 
     x.height = 1 + max(height(x.left), height(x.right))
     y.height = 1 + max(height(y.left), height(y.right))
+
+    return y
 
 # Function to get height of tree
 def height(node):
@@ -58,10 +60,78 @@ def insert(node, val):
 
     balance = getBalance(node)
 
+    if balance > 1 and val < node.left.val:
+        return rightRotate(node)
+    if balance < -1 and val > node.right.val:
+        return leftRotate(node)
+
+
+    if balance > 1 and val > node.left.val:
+        node.left = leftRotate(node.left)
+        return rightRotate(node)
+    
+    if balance < -1 and val < node.right.val:
+        node.right = rightRotate(node.right)
+        return leftRotate(node)
+    
+    return node
+
+def maxValueNode(node):
+    current = node
+    while current.right is not None:
+        current = current.right
+
+    return current
+
+def delete(node, val):
+    if not node:
+        return node
+    
+    if val < node.val:
+        node.left = delete(node.left, val)
+    elif val > node.val:
+        node.right = delete(node.right, val)
+    else:
+        if not node.left or not node.right:
+            temp = node.left if node.left else node.right
+
+            if not temp:
+                node = None
+            else:
+                node = temp
+
+        else:
+            temp = maxValueNode(node.left)
+            node.val = temp.val
+            node.left = delete(node.left, temp.val)
+
+    if not node:
+        return node
+    
+    node.height = 1 + max(height(node.left), height(node.right))
+
+    balance = getBalance(node)
+
+    if balance > 1 and getBalance(node.left) >= 0:
+        return rightRotate(node)
+    if balance > 1 and getBalance(node.left) < 0:
+        node.left = leftRotate(node.left)
+        return rightRotate(node)
+    
+    if balance < -1 and getBalance(node.right) <= 0:
+        return leftRotate(node)
+    
+    if balance < -1 and getBalance(node.right) > 0:
+        node.right = rightRotate(node.right)
+        return leftRotate(node)
+    
+
+    return node
+
 
 # Level order traversal, mainly for testing purposes
 def levelOrder(node):
-    if node == None:
+    if not node:
         return
     
     q = queue()
@@ -85,11 +155,24 @@ def levelOrder(node):
 
 # actual main code
 def main():
-    node = Node(5)
-    node.left = Node(3)
-    node.right = Node(7)
+    root = None
 
-    levelOrder(node)
+    vals = [10,20,30,40,50,25]
+
+    for i in vals:
+        root = insert(root, i)
+
+    print("Level order:")
+    levelOrder(root)
+    print("\n")
+
+    deletions = [20, 30]
+    for j in deletions:
+        root = delete(root, j)
+
+    print("Level order after deletions:")
+    levelOrder(root)
+    print("\n")
     
 
 # Main
