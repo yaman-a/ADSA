b08c5ae163a75cdf857d84022ce103e9455b1a8f
yaman-a <yaman.ashqar05@gmail.com>
2024-09-13 09:45:58 +0930

    commenting

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def rightRotate(a):
     # Return new node
     return b
 
+# Perform left rotation
 def leftRotate(x):
     y = x.right
     z = y.left
@@ -45,7 +46,7 @@ def height(node):
         return 0
     return node.height
 
-# Get the balance of node
+# Get the balance factor of node
 def getBalance(node):
     if not node:
         return 0
@@ -53,9 +54,11 @@ def getBalance(node):
 
 # Insert new value
 def insert(node, val):
+    # Base case
     if not node:
         return Node(val)
     
+    # Insert into left or right subtree using recursion
     if val < node.val:
         node.left = insert(node.left, val)
     elif val > node.val:
@@ -63,26 +66,32 @@ def insert(node, val):
     else:
         return node
     
+    # Update height
     node.height = 1 + max(height(node.left), height(node.right))
 
+    # Check balance of tree and perform rotations if unbalanced
     balance = getBalance(node)
 
+    # Left left case
     if balance > 1 and val < node.left.val:
         return rightRotate(node)
+    # Right right case
     if balance < -1 and val > node.right.val:
         return leftRotate(node)
 
-
+    # Left right case
     if balance > 1 and val > node.left.val:
         node.left = leftRotate(node.left)
         return rightRotate(node)
     
+    # Right left case
     if balance < -1 and val < node.right.val:
         node.right = rightRotate(node.right)
         return leftRotate(node)
     
     return node
 
+# find the node with the max value (rightmost node)
 def maxValueNode(node):
     current = node
     while current.right is not None:
@@ -90,44 +99,55 @@ def maxValueNode(node):
 
     return current
 
+# Delete value from tree
 def delete(node, val):
+    # Base case
     if not node:
         return node
     
+    # Search for value using recursion
     if val < node.val:
         node.left = delete(node.left, val)
     elif val > node.val:
         node.right = delete(node.right, val)
     else:
+        # Node is found
         if not node.left or not node.right:
             temp = node.left if node.left else node.right
 
             if not temp:
-                node = None
+                node = None # No child case
             else:
-                node = temp
+                node = temp # One child case
 
         else:
+            # Node has two children, get max value from left subtree
             temp = maxValueNode(node.left)
             node.val = temp.val
             node.left = delete(node.left, temp.val)
 
+    # If the tree only has one node
     if not node:
         return node
     
+    # Update height
     node.height = 1 + max(height(node.left), height(node.right))
 
+    # Get balance and perform rotations if unbalanced
     balance = getBalance(node)
 
+    # Left left case
     if balance > 1 and getBalance(node.left) >= 0:
         return rightRotate(node)
-    if balance > 1 and getBalance(node.left) < 0:
-        node.left = leftRotate(node.left)
-        return rightRotate(node)
-    
+    # Right right case
     if balance < -1 and getBalance(node.right) <= 0:
         return leftRotate(node)
     
+    # Left right case
+    if balance > 1 and getBalance(node.left) < 0:
+        node.left = leftRotate(node.left)
+        return rightRotate(node)
+    # Right left case
     if balance < -1 and getBalance(node.right) > 0:
         node.right = rightRotate(node.right)
         return leftRotate(node)
@@ -136,7 +156,7 @@ def delete(node, val):
     return node
 
 
-# Level order traversal, for testing purposes only
+# For testing purposes only: Level order traversal (BFS)
 def levelOrder(node):
     if not node:
         return
@@ -160,30 +180,37 @@ def levelOrder(node):
 
             print(curr.val, end=" ")
 
+# DFS methods
+# Preorder traversal (node, left, right)
 def preOrder(root):
     if not root:
         return []
     
     return [root.val] + postOrder(root.left) + postOrder(root.right)
 
+# Postorder traversal (left, right, node)
 def postOrder(root):
     if not root:
         return []
     
     return postOrder(root.left) + postOrder(root.right) + [root.val]
 
+# Inorder traversal (left, node, right)
 def inorder(root):
     if not root:
         return []
     
     return inorder(root.left) + [root.val] + inorder(root.right)
 
-# actual main code
+# Main code, handles input and creates tree
 def main():
+    # Empty tree
     root = None
 
+    # Parse input and split commands
     inputLine = input().strip().split()
 
+    # Loop through commands except the last one
     for command in inputLine[:-1]:
         if command[0] == 'A':
             val = int(command[1])
@@ -192,8 +219,10 @@ def main():
             val = int(command[1])
             root = delete(root, val)
 
+    # Last input, which is traversal type
     traversal = inputLine[-1]
 
+    
     if not root:
         print("EMPTY")
     else:
